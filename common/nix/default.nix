2be0f75be760d1daac46989dabde16b0e39b86a3
Alex Jackson <git@ajaxbits.com>
2026-03-26 09:33:36 -0400

    feat: use lixPackageSets.latest for everything

--- common/nix/default.nix
+++ common/nix/default.nix
@@ -1,15 +1,22 @@
 { pkgs, ... }:
+let
+  lixPackageSet = pkgs.lixPackageSets.latest;
+in
 {
   nixpkgs.overlays = [
-    (final: prev: {
-      inherit (prev.lixPackageSets.stable)
+    (_: _: {
+      inherit (lixPackageSet)
         colmena
+        nil
+        nix-direnv
         nix-eval-jobs
         nix-fast-build
         nixpkgs-review
+        nixpkgs-reviewFull
+        nurl
         ;
     })
   ];
 
-  nix.package = pkgs.lixPackageSets.stable.lix;
+  nix.package = lixPackageSet.lix;
 }
